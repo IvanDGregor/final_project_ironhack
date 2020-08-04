8d6788c397c1002a246961ed54512c720b1821b5
IvanDGregor <ivan.gregorio.93@gmail.com>
2020-08-05 00:18:58 +0200

    Delete some components front

--- front-final-project/src/app/app-routing.module.ts
+++ front-final-project/src/app/app-routing.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LeadListComponent } from './lead-list/lead-list.component';
-import { OpportunityListComponent } from './opportunity-list/opportunity-list.component';
 import { CreateFormComponent } from './create-form/create-form.component';
 import { LoginComponent } from './login/login.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { AccountListComponent } from './account-list/account-list.component';
-import { ConvertLeadComponent } from './convert-lead/convert-lead.component';
-import { CreateLeadComponent } from './create-lead/create-lead.component';
-import { CreateSalesrepComponent } from './create-salesrep/create-salesrep.component';
 import { AuthGuard } from './_helpers';
-import { ProductReportComponent } from './product-report/product-report.component';
-import { IndustryReportComponent } from './industry-report/industry-report.component';
-import { CountryReportComponent } from './country-report/country-report.component';
-import { CityReportComponent } from './city-report/city-report.component';
-import { StatsReportComponent } from './stats-report/stats-report.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateUserComponent } from './create-user/create-user.component';
-import { SalesrepReportComponent } from './salesrep-report/salesrep-report.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { CreateCreditCardComponent } from './create-credit-card/create-credit-card.component';
 import { CreditCardListComponent } from './credit-card-list/credit-card-list.component';
@@ -35,24 +24,6 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'leads',
-    component: LeadListComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'convert-lead/:id',
-    component: ConvertLeadComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'opportunities',
-    component: OpportunityListComponent,
     pathMatch: 'full',
     canActivate: [AuthGuard],
   },
@@ -93,12 +64,6 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: 'create-lead',
-    component: CreateLeadComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
     path: 'create-user',
     component: CreateUserComponent,
     pathMatch: 'full',
@@ -119,48 +84,6 @@ const routes: Routes = [
   {
     path: 'create-credit-card',
     component: CreateCreditCardComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'create-salesrep',
-    component: CreateSalesrepComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/salesrep',
-    component: SalesrepReportComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/product',
-    component: ProductReportComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/industry',
-    component: IndustryReportComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/country',
-    component: CountryReportComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/city',
-    component: CityReportComponent,
-    pathMatch: 'full',
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'report/stats',
-    component: StatsReportComponent,
     pathMatch: 'full',
     canActivate: [AuthGuard],
   },
--- front-final-project/src/app/app.module.ts
+++ front-final-project/src/app/app.module.ts
@@ -8,23 +8,12 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LeadListComponent } from './lead-list/lead-list.component';
-import { OpportunityListComponent } from './opportunity-list/opportunity-list.component';
 import { CreateFormComponent } from './create-form/create-form.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { AccountListComponent } from './account-list/account-list.component';
 import { BasicAuthInterceptor, ErrorInterceptor } from './_helpers';
-import { ConvertLeadComponent } from './convert-lead/convert-lead.component';
-import { CreateLeadComponent } from './create-lead/create-lead.component';
-import { CreateSalesrepComponent } from './create-salesrep/create-salesrep.component';
-import { SalesrepReportComponent } from './salesrep-report/salesrep-report.component';
-import { CountryReportComponent } from './country-report/country-report.component';
-import { CityReportComponent } from './city-report/city-report.component';
-import { IndustryReportComponent } from './industry-report/industry-report.component';
-import { ProductReportComponent } from './product-report/product-report.component';
-import { StatsReportComponent } from './stats-report/stats-report.component';
 import { ToastrModule } from 'ngx-toastr';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateUserComponent } from './create-user/create-user.component';
@@ -40,22 +29,11 @@ import { CreateTransferComponent } from './create-transfer/create-transfer.compo
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    LeadListComponent,
-    OpportunityListComponent,
     CreateFormComponent,
     LoginComponent,
     LayoutComponent,
     ContactListComponent,
     AccountListComponent,
-    ConvertLeadComponent,
-    CreateLeadComponent,
-    CreateSalesrepComponent,
-    SalesrepReportComponent,
-    CountryReportComponent,
-    CityReportComponent,
-    IndustryReportComponent,
-    ProductReportComponent,
-    StatsReportComponent,
     DashboardComponent,
     CreateUserComponent,
     CreateAccountComponent,
